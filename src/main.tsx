6538514c8069dc3cfb08322cb71940a527382a46
Fiona Lewis <hwindepth@yahoo.com>
2024-05-27 13:12:19 +0100

    2nd commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App.tsx'
+import App from './app/layout/App.jsx'
 import './app/layout/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
